171a340551e6c5a0f484d95896df5ce3c9931ba0
Beto Magno <robertomagnogarcia@gmail.com>
2022-02-15 15:00:48 -0300

    Criação tela update e delete

--- Geekshoping/GeekShopping.Web/Controllers/ProductController.cs
+++ Geekshoping/GeekShopping.Web/Controllers/ProductController.cs
@@ -33,13 +33,55 @@ namespace GeekShopping.Web.Controllers
             {
                 var response = await _productService.CreateProduct(model);
 
-                if (response != null) return RedirectToAction("ProductIndex");
+                if (response != null) return RedirectToAction(nameof(ProductIndex));
 
 
             }
 
-           
+
             return View(model);
         }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var model = await _productService.FindProductById(id);
+
+            if (model != null) return View(model);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(model);
+                if (response != null) return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var model = await _productService.FindProductById(id);
+
+            if (model != null) return View(model);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+
+            var response = await _productService.DeleteProductById(model.Id);
+            if (response) return RedirectToAction(nameof(ProductIndex));
+
+            return View(model);
+        }
+
     }
 }
